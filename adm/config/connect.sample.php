3dec699ba7eda37e6e9a032986fbb7022f59dd46
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-04-19 20:36:05 +0700

    update config

--- adm/config/connect.sample.php
+++ adm/config/connect.sample.php
@@ -1,8 +1,17 @@
 <?php
+define('N',$_SERVER['SERVER_NAME']);
+define('A',$_SERVER['SERVER_ADDR']);
+define('REQURI', $_SERVER['REQUEST_URI']);
+define('ENV', 'Development');
+define('ROOT',dirname(__FILE__));
+define('PHOTO',ROOT.'/p');
+define('SITEURL','http://'.N.'/papah-racing');
+define('TEMA',SITEURL.'/tema');
+
 $dbhost = 'localhost';
-$dbuser = 'u1039423_andre';
-$dbpass = 'Papahracing2000';
-$dbname = 'u1039423_papahracing';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'papah_racing';
 $db_fc = "mysql:dbname=$dbname;host=$dbhost";
 try {
     $db = new PDO($db_fc, $dbuser, $dbpass);
