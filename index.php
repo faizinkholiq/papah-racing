f8876212eabece04b1b797f1c8af8c87510190e6
faizin <nasrunfaizinkholiq@gmail.com>
2023-01-18 23:21:13 +0700

    fix footer

--- index.php
+++ index.php
@@ -330,7 +330,6 @@ function foot(){
 	</div>'.
 	'<div class="address mt-3">'.str_replace("\n",'<br/>',$alamat).'</div>'.
 	'</div></div></div></div></div>'.
-	'</div>'.
 	'<div class="footer-bottom"><div class="container"><div class="row align-items-center"><div class="col-lg-12 col-md-12 text-center"><p class="mb-0">© 2021 '.$toko.' - '.$ket.'.</p></div></div></div></div>'.'</footer>';
 			
 	/* ------------------------------- */
