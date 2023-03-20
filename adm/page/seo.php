7927d61dbd8698115fefda448e2980bf863a8778
faizin <nasrunfaizinkholiq@gmail.com>
2023-03-20 11:33:32 +0700

    updatew seo

--- adm/page/seo.php
+++ adm/page/seo.php
@@ -36,14 +36,15 @@ $get_pelanggan = mysqli_query($con, "
     </div>
     <div class="col-4"><a href="main?url=supplier" class="btn btn-danger float-right"><i class='fas fa-times-circle mr-2'></i>Back</a></div>
 </div>
-<div class="wrapper">
-    <div class="row">
+
+<div class="wrapper mt-2">
+    <div class="row" style="font-size: 1.3rem;">
         <div class="col-sm-2"><i class="fas fa-users mr-2"></i>Total Visitor</div>
         <div class="col-sm-10"><?= isset($data['visitor'])? $data['visitor'] : 0; ?></div>
     </div>
 </div>
 
-<div class="wrapper">
+<div class="wrapper mt-2">
     <h4>Barang Paling Banyak Dibeli</h4>
     <div class="table-responsive mt-3">
         <table class="table table-striped table-bordered display" style="width:100%">
@@ -70,7 +71,7 @@ $get_pelanggan = mysqli_query($con, "
     </div>
 </div>
 
-<div class="wrapper">
+<div class="wrapper mt-2">
     <h4>Pelanggan Paling Banyak melakukan Pembelian</h4>
     <div class="table-responsive mt-3">
         <table class="table table-striped table-bordered display" style="width:100%">
